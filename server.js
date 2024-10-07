4a66d05f4cf096f31174ca22abc06c00e82225b6
GregCuevas <gregorycuevas12@icloud.com>
2024-10-06 19:50:09 -0600

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import sendIndividualHandler from "./api/send-individual.js";
 import dotenv from "dotenv";
 
@@ -6,6 +7,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Configurar CORS para permitir solicitudes desde cualquier origen
+app.use(
+  cors({
+    origin: "https://www.coopebred.com", // Reemplaza con tu dominio permitido
+    methods: "POST",
+    allowedHeaders: "Content-Type",
+  })
+);
 
 // Ruta para manejar el envío del formulario
 app.post("/api/send-individual", sendIndividualHandler);
