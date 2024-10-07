bdb06b2e87569dd45ee7675bd54c5d180300b24a
GregCuevas <gregorycuevas12@icloud.com>
2024-10-06 19:57:59 -0600

    sendIndividuals

--- api/send-individual.js
+++ api/send-individual.js
@@ -59,8 +59,8 @@ export default async function handler(req, res) {
         // Envío del correo utilizando Resend
         const resend = new Resend(process.env.RESEND_API_KEY);
         await resend.emails.send({
-          from: "onboarding@resend.dev",
-          to: "onboarding@resend.dev",
+          from: "coopebred.com",
+          to: "gregoryjunior2101@gmail.com",
           subject: `Nuevo mensaje de ${nombres || "socio-individual"}`,
           html: htmlContent,
         });
